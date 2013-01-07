20b6554759833f94befc75b9f0537771e7afb17a
Nikolay Markov <mainameiz@gmail.com>
2013-01-07 04:21:29 +0400

    New setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+make_backup() {
+  backup_name="${1}"
+  backup_number="${2}"
+  backup_file="${backup_name}.${backup_number}.bak"
+  if [[ -e "$backup_file" ]]; then
+    backup_number=$(( $backup_number + 1 ))
+    make_backup "$backup_name" "$backup_number"
+  else
+    cp "$backup_name" "$backup_file"
+  fi
+}
+
 REPO_DIR=".dotfiles"
 REPO="${HOME}/${REPO_DIR}"
 GIT_DIR="--git-dir=${REPO}/.git --work-tree=${REPO}/"
 
 if [[ -d "${REPO}/.git" ]]; then
   echo "Updating repository..."
-  git $GIT_DIR pull
+  git $GIT_DIR pull origin master
 else
   echo "Cloning repository..."
   git clone https://github.com/mainameiz/dotfiles.git "${REPO}"
@@ -14,67 +26,21 @@ fi
 
 FILES=$(git $GIT_DIR ls-tree --name-only HEAD files/)
 
-change="n" # изменить ссылку?
-backup="y" # сделать бэкап существующего файла?
-
 cd ~
 echo " Symlinking..."
 for ITEM in ${FILES}; do
-  FILE="$REPO_DIR/$ITEM"
+  FILE="$REPO_DIR/$ITEM" # .dotfiles/files/file
   bname=$(basename "$ITEM")
-  if [[ -e "$bname" ]]; then # файл существует?
-    update="y"
-    if [[ -d "$bname" ]]; then
-      link_path=$(readlink -f "$bname")
-      if [[ "$link_path" == "$HOME/$FILE" ]]; then
-        update="n"
-      fi
-    else
+  if [[ -e "$bname" && ! -h "$bname" ]]; then # если файл существует и он не символьная ссылка
+    if [[ -f "$bname" && -f "$HOME/$FILE" ]]; then # если это файлы - сравнить и сделать бэкап, если нужно
       cmp "$bname" "$HOME/$FILE"
-      if [[ "$?" == "0" ]]; then
-        update="n"
+      if [[ "$?" != "0" ]]; then
+        make_backup "$bname"
       fi
+    else # если один из источников не файл то, сделать бэкап в любом случае
+      make_backup "$bname"
     fi
-    if [[ "$update" == "n" ]]; then
-      backup="n"
-      change="n"
-    else
-
-      echo -n "File $bname exists. Do you want to change it and make a backup? [yes/no]: "
-      while true; do
-        read NSWR
-        case $NSWR in
-          y|ye|yes)
-            backup="y"
-            change="y"
-            break
-            ;;
-          n|no)
-            backup="n"
-            change="n"
-            break
-            ;;
-          *)
-            echo -n "Sorry, possible answers are \"yes\" or \"no\" [yes/no]: "
-            ;;
-        esac
-      done
-      unset NSWR
-
-    fi
-  else
-    backup="n"
-    change="y"
   fi
-  if [[ "$backup" == "y" ]]; then
-    timestamp=$(($(date +%s%N)/1000000))
-    mv "$bname" "$REPO/backups/$bname-$timestamp.bkp"
-  fi
-  if [[ "$change" == "y" ]]; then
-    rm -rf "$bname"
-    ln -sv "$FILE"
-  else
-    echo "Skipped: $bname"
-  fi
+  ln -svf "$FILE"
 done
 cd -
